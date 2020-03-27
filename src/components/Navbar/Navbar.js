44b5dfb4aad643bd331e18b136165c4df8e76b9c
Lwin Moe Paing <lwinmoepaing@marathonmyanmar.com>
2020-03-27 17:55:49 +0630

    Add Profile List

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import classes from './Navbar.css'
+import classes from './Navbar.scss'
 
 const Navbar = () => {
 	return (
-		<div className={classes.Power}>
-			Navbar
+		<div className={[classes.Navbar]}>
+			Lapyae Hour 50 <span className="mm-font"> ကံစမ်းမဲ </span>
 		</div>
 	)
 }
